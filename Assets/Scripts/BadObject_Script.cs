114efbfb034bbf7766348c7ebfbb858859d73f16
rakinola-citytech <rukayat.akinola@mail.citytech.cuny.edu>
2024-02-22 15:42:01 -0500

    02/22/2024
    Please review my progress.

--- Assets/Scripts/BadObject_Script.cs
+++ Assets/Scripts/BadObject_Script.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class BadObject_Script : MonoBehaviour
 {
-    private float yMove = -2;
+    private float yMove = -3;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(0, yMove * Time.deltaTime, 0);
+        
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -23,9 +24,10 @@ public class Item : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if ((collision.tag == "Player") || (collision.tag == "Ground"))
+        if (collision.tag == "Player")
         {
             Destroy(gameObject);
+            Debug.Log("Bad Object");
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,36 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //public variables are controlled in unity's inspector
-    public float speed = 5;
-    float turboSpeed = 10;
-    float currentSpeed;
-    SpriteRenderer sr;
-    public Color turboColor;
-
+    private float speed = 5;
+  
     //start is called before the first frame update;
     //think pre-game
     void Start()
     {
         //controls
-        sr = GetComponent<SpriteRenderer>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space)){
-            currentSpeed = turboSpeed;
-            sr.color = turboColor;
-        }
-        else {
-            currentSpeed = speed;
-            sr.color = Color.white;
-        }
-
-        float xMove = Input.GetAxis("Horizontal");
-        float yMove = Input.GetAxis("Vertical");
-        transform.Translate(currentSpeed * xMove * Time.deltaTime, currentSpeed * yMove * Time.deltaTime, 0);
-
+        float xMove = Input.GetAxisRaw("Horizontal");
+        transform.Translate(speed * xMove * Time.deltaTime, 0, 0);
     }
 }
